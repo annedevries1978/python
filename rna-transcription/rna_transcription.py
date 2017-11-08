774c64ab144725c6e36d11fbfb0b490262ed5b1d
annedevries1978 <annedevries1978@gmail.com>
2017-11-08 21:54:10 +0100

    Exercism

---
+++ rna-transcription/rna_transcription.py
@@ -0,0 +1,31 @@
+'''Given a DNA strand, its transcribed RNA strand is formed by replacing
+each nucleotide with its complement:
+
+* `G` -> `C`
+* `C` -> `G`
+* `T` -> `A`
+* `A` -> `U`
+
+Your function will need to be able to handle invalid inputs by raising a
+`ValueError` with a meaningful message.'''
+
+import re
+
+
+def to_rna(dna_strand):
+    valid_input = ['G', 'C', 'T', 'A']
+    coding = {'G': 'C',
+              'C': 'G',
+              'T': 'A',
+              'A': 'U'}
+    rna_strand = ''
+    for x in dna_strand:
+        if x not in valid_input:
+            raise ValueError('Fout')
+        else:
+            for char in dna_strand:
+                rna_strand += coding[char]
+            return rna_strand
+
+
+print(to_rna('BAXXXCTTAA'))
