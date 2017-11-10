ec7d25e4a9d1e6b3cd1b0af661f00bff0f35cb61
annedevries1978 <annedevries1978@gmail.com>
2017-11-10 21:27:37 +0100

    Exercism run length encoding

---
+++ run-length-encoding/run_length_encoding.py
@@ -0,0 +1,17 @@
+def decode(string):
+    pass
+
+
+def encode(string):
+    counter = 0
+    encoded = ''
+    for i in range(len(string)):
+        print(string[i])
+        if string[i] == string[+1]:
+            counter += 1
+        encoded +
+
+
+
+
+print(encode('AACCCCCCAAAAAAA'))
