bb39f8fe18a28fffa7d6d37dfaf9c65109c49db5
annedevries1978 <annedevries1978@gmail.com>
2017-11-12 10:08:28 +0100

    Exercism run length encoding

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,17 +1,35 @@
 def decode(string):
-    pass
+    decoded = ""
+    for i in range(len(string)):
+        if string[i].isnumeric():
+            if string[i + 1].isnumeric():
+                decoded += string[i + 2] * (int(string[i] + string[i + 1]) - 1)
+            elif not string[i - 1].isnumeric():
+                decoded += string[i + 1] * (int(string[i]) - 1)
+        else:
+            decoded += string[i]
+    return decoded
 
 
 def encode(string):
-    counter = 0
-    encoded = ''
-    for i in range(len(string)):
-        print(string[i])
-        if string[i] == string[+1]:
-            counter += 1
-        encoded +
+    if string is "":
+        return string
+
+    compression = ""
+    letter = string[0]
+    count = 1
+
+    for char in string[1:]:
+        if char == letter:
+            count += 1
+        else:
+            compression += (str(count) if count != 1 else "") + letter
+            letter = char
+            count = 1
+
+    compression += (str(count) if count != 1 else "") + letter
+
+    return compression
 
 
-
-
-print(encode('AACCCCCCAAAAAAA'))
+print(decode('2A2B2C'))
