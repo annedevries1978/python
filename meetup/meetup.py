0e3ceae274bf5c900a0a8213976c4f2526d9a170
annedevries1978 <annedevries1978@gmail.com>
2017-11-13 22:09:10 +0100

    Exercism run length encoding

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -2,32 +2,18 @@ from datetime import date
 import calendar
 
 def meetup_day(year, month, day_of_the_week, which):
-    if day_of_the_week == 'Monday':
-        day_of_the_week = 1
-    print (calendar.weekday(year, month, day_of_the_week))
-
-
+    month = calendar.monthcalendar(2013, 5)
+    # get the week day
+    week_days = [x for x in calendar.day_name]
+    for day_of_the_week in week_days:
+        week_day_int = week_days.index(day_of_the_week)
 
 
 
 meetup_day(2013, 5, 'Monday', 'teenth')
 #antwoord is 13 mei
-week_days = [x for x in calendar.day_name]
 # print(week_days)
 # print(calendar.monthcalendar(2013, 5))
-# print(calendar.monthcalendar(2013, 6))
-month = calendar.monthcalendar(2013, 5)
-
-def monthDayName(month):
-    day_week_name = []
-    for week in month:
-        for week_day in week:
-            if week_day != 0:
-
-                month_day = week_day
-                month_day_index = week_days[week.index(week_day)]
-                day_week_name.append([week_day, month_day_index])
-    return day_week_name
-
-
-print(monthDayName(month))
+month = calendar.monthcalendar(2013, 5)[1]
+# month = calendar.monthrange(2013, 5)[1]
+print(month)
