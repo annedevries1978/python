f26ea14eac6d5295bb73140ddfc7cd575dc2a06a
annedevries1978 <annedevries1978@gmail.com>
2017-11-16 21:39:37 +0100

    Exercism anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -4,19 +4,13 @@ def detect_anagrams(word, candidates):
     for item in candidates:
         temp_word = word.lower()
         item_to_test = item.lower()
-        # only compare if length are equal
+        # only compare if length are equal but words are not
         if temp_word != item_to_test and len(item) == len(word):
-
+            print(sorted(temp_word), sorted(item_to_test))
             # Loop over letters in item
-            for x in range(0, len(item)):
-                # if letter found in word strip word of letter
-                if item_to_test[x] in temp_word:
-                    #print("found letter:", item[x])
-                    temp_word = temp_word.replace(item_to_test[x], "", 1)
-
-                    # all letters in item are in word
-                    if len(temp_word) == 0:
-                        matches.append(item)
-
-
+            # use sorted instead of for loop
+            if sorted(temp_word) == sorted(item_to_test):
+                matches.append(item)
     return matches
+
+print(detect_anagrams('master', ["stream", "pigeon", "maters"]))
