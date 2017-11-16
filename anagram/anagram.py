2ebdc41a3e91597d68ff7104b9faed41cd45bf58
annedevries1978 <annedevries1978@gmail.com>
2017-11-16 21:29:24 +0100

    Exercism anagram

---
+++ anagram/anagram.py
@@ -0,0 +1,24 @@
+import re
+
+def detect_anagrams(word, candidates):
+    matches = []
+    # loop over items in candidates
+    for item in candidates:
+        temp_word = word.lower()
+        # only compare if length are equal
+        item_to_test = item.lower()
+        if temp_word != item_to_test and len(item) == len(word):
+
+            # Loop over letters in item
+            for x in range(0, len(item)):
+                # if letter found in word strip word of letter
+                if item_to_test[x] in temp_word:
+                    #print("found letter:", item[x])
+                    temp_word = temp_word.replace(item_to_test[x], "", 1)
+
+                    # all letters in item are in word
+                    if len(temp_word) == 0:
+                        matches.append(item)
+
+
+    return matches
