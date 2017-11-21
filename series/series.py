3590b1b4be177d81acac3a3e9d6d0b5ed469213e
annedevries1978 <annedevries1978@gmail.com>
2017-11-21 22:53:05 +0100

    Exercism series

--- series/series.py
+++ series/series.py
@@ -1,13 +1,21 @@
 def slices(series, length):
     start = 0
     end = length
-    for x, y in enumerate(series):
-        while len(series[start:end]) == length:
-            # print(x, x+1)
-            result = series[start:end]
-            end += 1
-            start += 1
-            print (result)
+    result_list = []
+    if length == 0 or length > len(series):
+        raise ValueError
+    else:
+        for x, y in enumerate(series):
+            if length == len(series[start:end]):
+                # print(x, x+1)
+                result = series[start:end]
+                end += 1
+                start += 1
+                z =[int(a) for a in list(result)]
+                result_list.append(z)
+
+        return result_list
 
 
-print(slices('2346780', 1))
+#print(slices('2346780', 10))
+print(slices("01234", 0))
