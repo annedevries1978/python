e760092dfd724995d632fac500fcd81a32797bb9
annedevries1978 <annedevries1978@gmail.com>
2017-11-26 22:10:14 +0100

    Exercism atbash-cipher

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,13 +1,22 @@
+import re
+
 def encode(plain_text):
     plain = 'abcdefghijklmnopqrstuvwxyz'
     cipher = 'zyxwvutsrqponmlkjihgfedcba'
 
-    trantab = plain_text.maketrans(plain, cipher)
+    #remove punctuation
+    regex = re.compile('[!" ".,]')
+    text_without_puntuation = regex.sub('', plain_text).lower()
+    trantab = text_without_puntuation.maketrans(plain, cipher)
+    encoded_text = text_without_puntuation.translate(trantab)
+    test = [encoded_text[i:i+5] for i in range(0, len(encoded_text), 5)]
+    return " ".join(test)
 
-    return plain_text.translate(trantab)
 
 def decode(ciphered_text):
-    pass
-
-print(encode('abcdeabcde'))
+    plain = 'abcdefghijklmnopqrstuvwxyz'
+    cipher = 'zyxwvutsrqponmlkjihgfedcba'
+    trantab = ciphered_text.maketrans(cipher, plain)
+    plain_text = ciphered_text.translate(trantab)
+    return plain_text.replace(" ", "")
 
