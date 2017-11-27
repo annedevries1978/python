9165968fdbdf81bd3994d2d0be1f19f6b75f29ab
annedevries1978 <annedevries1978@gmail.com>
2017-11-27 21:50:22 +0100

    Exercism acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -3,7 +3,7 @@ import re
 def abbreviate(words):
     abbreviation = ""
     x = re.compile(r'\W+')
-    word_list = x.split(words)  # create a list of the words
+    word_list = x.split(words)  # create a list of all the words
     for a, b in enumerate(word_list):
         abbreviation += word_list[a][0].upper()  # get first letter of each word and capitalize
     return abbreviation
