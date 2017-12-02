0b3ad86f6a3d20216406c9b4aee1e51763aacbd8
annedevries1978 <annedevries1978@gmail.com>
2017-12-02 09:53:45 +0100

    Exercism say

--- say/say.py
+++ say/say.py
@@ -1,34 +1,45 @@
 def say(number):
 
     basic = {1: 'one', 2: 'two',
-                  3: 'three', 4: 'four',
-                  5: 'five', 6: 'six',
-                  7: 'seven', 8: 'eight',
-                  9: 'nine'}
+             3: 'three', 4: 'four',
+             5: 'five', 6: 'six',
+             7: 'seven', 8: 'eight',
+             9: 'nine',
+             10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen',
+             14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen',
+             18: 'eighteen', 19: 'nineteen'
+             }
 
-    tens = {10: 'ten', 20: 'twenty',
-            30: 'thirty', 40: 'forty',
-            50: 'fifty', 60: 'sixty',
-            70: 'seventy', 80: 'eighty',
-            90: 'ninety'}
+    ten_numbers = {20: 'twenty',
+                   30: 'thirty', 40: 'forty',
+                   50: 'fifty', 60: 'sixty',
+                   70: 'seventy', 80: 'eighty',
+                   90: 'ninety'
+                   }
 
-    exotic = {11: 'eleven', 12: 'twelve',
-              13: 'thirteen'}
-
-    teen = {}
-
-    # split to tenth
-    tenth = int(number/10)
-
-    if tenth < 1:
+    if number == 0:
+        return 'zero'
+    elif number in ten_numbers.keys():
+        return ten_numbers[number]
+    elif 1 <= number < 20:
         return basic[number]
+    elif 20 <= number < 100:
+        tens, below_ten = divmod(number, 10)
+        return ten_numbers[tens*10] + "-" + basic[below_ten]
+    else:
+        return "Number not in Range!!!"
 
-    elif number % 10 == 0:
-        return tens[number]
 
-    if number in exotic.keys():
-        return exotic[number]
+def number_split(number):
+    pass
 
-
-print(say(70))
 print(say(13))
+print(say(0))
+print(say(5))
+print(say(42))
+print(say(50))
+print(say(-1))
+print(say(100))
+
+
+
