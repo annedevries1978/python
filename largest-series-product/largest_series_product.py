a2fb3ad4aba5866a7314ddc2893193d50627e1e6
Anne de Vries <annedevries1978@gmail.com>
2017-12-13 20:35:53 +0100

    Exercism largest series product

---
+++ largest-series-product/largest_series_product.py
@@ -0,0 +1,30 @@
+def largest_product(series, size):
+    start = 0
+    end = size
+    result_list = []
+    if size > len(series):
+        raise ValueError
+    elif size == 0:
+        return 1
+    else:
+        for x, y in enumerate(series):
+            if size == len(series[start:end]):  # series length equals size
+                # print(x, x+1)
+                result = series[start:end]
+                end += 1
+                start += 1
+                num_list = [int(a) for a in list(result)]
+                result_list.append(num_list)
+        return multiply_nested_list(result_list)
+
+
+def multiply_nested_list(nested_list):
+    result = [product(i) for i in nested_list]
+    return max(result)
+
+
+def product(num_list):
+    product = 1
+    for number in num_list:
+        product *= number
+    return product
