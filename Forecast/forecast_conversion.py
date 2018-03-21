316723eaf4151a73f37f104b1fbb3970070bede8
Anne <annedevries1978@gmail.com>
2018-03-21 21:59:04 +0100

    Forecast

--- Forecast/forecast_conversion.py
+++ Forecast/forecast_conversion.py
@@ -1,55 +1,70 @@
 import csv
 import pprint
-
 '''
     Data uit forecast bestand van Baxi omzetten naar een voor SAP leesbaar format
     De kolomnamen beginnen bij regel 14
     Vanaf regel 67 begint een nieuwe pagina
 '''
 pp = pprint.PrettyPrinter()
+start_line = 14
+last_line = 65
+forecast_data = []
 
 
-def convert_spaces_to_comma():
-    with open('C:\\Users\\anv\\Desktop\\baxi.txt', newline='') as f, open('C:\\Users\\anv\\Desktop\\format_test.txt',
-                                                                          '+w', newline='') as f2:
-        reader = csv.reader(f, delimiter=' ')
-        writer = csv.writer(f2)
-        for row in reader:
-            writer.writerow(row)
-
-
-def remove_duplicate_comma():
-    y = []
-    with open('C:\\Users\\anv\\Desktop\\format_test.txt') as f:
-        for line in f:
-            # loop over characters in de lijn 
-            for x in (range(len(line))):
-                print(line[x], end='')
-
-
-def open_forecast_file():
-    forecast_data = []
+def get_column_headers():
     line_counter = 0
-    with open('C:\\Users\\anv\\Downloads\\PSPSCD75RX (2).txt', newline='') as f:
+    with open('PSPSCD75RX (2).txt', newline='') as f:
         for line in f:
             line_counter += 1
             if line_counter == 14:  # eerste regel met kolomnamen
                 customer_item = line[:16]
                 rubitech_item = line[16:46]
-                forecast_data.append([customer_item, rubitech_item])
-                '''forecast_data.append(line[140:149])  # Order/Forecast
-                forecast_data.append(line[155:163])  # aantallen
-                forecast_data.append(line[165:173])  # datum'''
-
-            elif line_counter == 15:  # eerste regel met item data
-                customer_item = line[:16]
-                rubitech_item = line[16:46]
-                forecast_data.append([customer_item, rubitech_item])
-            elif line_counter > 15 and line not in [' \n\r']:
-                forecast_data.append([customer_item, rubitech_item])
-
-            print(line_counter)
-    pp.pprint(forecast_data)
+                aantallen = line[155:163]
+                datum = line[165:173]
+                forecast_data.append([customer_item, rubitech_item, aantallen, datum])
+    #return forecast_data
 
 
-open_forecast_file()
+def get_new_line_items():
+    # geeft de regelnummers waarin een nieuw item begint
+    line_counter = 0
+    start_line_new_item = [15]
+    with open('PSPSCD75RX (2).txt', newline='') as f:
+        for line in f:
+            line_counter += 1
+            if line == ' \r\n' and start_line < line_counter < last_line:
+                start_line_new_item.append(line_counter + 1)
+    return start_line_new_item
+
+
+# TODO over de regels loopen tot aan nieuw item. Item nummers kopieren in neuwe lijst
+def get_item_numbers():
+    #  per nieuwe regel de item nummers ophalen.
+    items = []
+    line_counter = 0
+    new_line_number = get_new_line_items()
+    with open('PSPSCD75RX (2).txt', newline='') as f:
+        for line in f:
+            line_counter += 1
+            for x in range(len(new_line_number)):
+                if line_counter == new_line_number[x]:
+                    customer_item = line[:16]
+                    rubitech_item = line[16:46]
+                    items.append([customer_item, rubitech_item])
+        return items
+
+
+def forecast_values():
+    line_counter = 0
+    new_line_number = get_new_line_items()
+    with open('PSPSCD75RX (2).txt', newline='') as f:
+        for line in f:
+            line_counter += 1
+            if 15 < line_counter < 36:
+                forecast_data.append(get_item_numbers()[0] + [line[155:163]])
+
+
+get_column_headers()
+forecast_values()
+pp.pprint(forecast_data)
+
