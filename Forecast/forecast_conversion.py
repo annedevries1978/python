85fcca001908a042af00a1ea722854722b7c27d3
Anne <annedevries1978@gmail.com>
2018-03-22 21:21:18 +0100

    Forecast

--- Forecast/forecast_conversion.py
+++ Forecast/forecast_conversion.py
@@ -6,9 +6,9 @@ import pprint
     Vanaf regel 67 begint een nieuwe pagina
 '''
 pp = pprint.PrettyPrinter()
-start_line = 14
-last_line = 65
-forecast_data = []
+start_line = 14  # start regel vanaf de data gelezen moet worden
+last_line = 65  # laatste regel
+forecast_data = []  # list om de data in op te slaan
 
 
 def get_column_headers():
@@ -22,18 +22,18 @@ def get_column_headers():
                 aantallen = line[155:163]
                 datum = line[165:173]
                 forecast_data.append([customer_item, rubitech_item, aantallen, datum])
-    #return forecast_data
 
 
 def get_new_line_items():
     # geeft de regelnummers waarin een nieuw item begint
     line_counter = 0
-    start_line_new_item = [15]
+    start_line_new_item = [15]  # regel met eerste item
     with open('PSPSCD75RX (2).txt', newline='') as f:
         for line in f:
             line_counter += 1
             if line == ' \r\n' and start_line < line_counter < last_line:
                 start_line_new_item.append(line_counter + 1)
+
     return start_line_new_item
 
 
@@ -60,12 +60,14 @@ def forecast_values():
         for line in f:
             line_counter += 1
             for x in range(len(new_line_number)):
-                if new_line_number[1] <= line_counter < new_line_number[2]:
-                    forecast_data.append(get_item_numbers()[0] + [line[155:163]] + [line[165:173]])
+                if x < 3:
+                    if new_line_number[x] <= line_counter < new_line_number[x+1]:
+                        forecast_data.append(get_item_numbers()[x] + [line[155:163]] + [line[165:173]])
 
-    for x in range(len(new_line_number)):
-        print(x)
 
+print(get_new_line_items())
+print(len(get_item_numbers()))
+pp.pprint(get_item_numbers())
 get_column_headers()
 forecast_values()
 pp.pprint(forecast_data)
